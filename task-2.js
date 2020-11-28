4fb8c494ae1feca2bc30e5362b68ef56a9ca3927
Mykhailo-Gogol <68640759+Mykhailo-Gogol@users.noreply.github.com>
2020-11-28 21:11:57 +0200

    Додає список #gallery, рішення для task-2

--- task-2.js
+++ task-2.js
@@ -7,8 +7,9 @@ const ingredients = [
   "Приправи",
 ];
 
-let rootContainerRef = document.querySelector("#gallery");
+let rootContainerRef = document.querySelector("#ingredients");
 rootContainerRef.style.marginTop = "50px";
+rootContainerRef.style.marginBottom = "50px";
 
 for (let i = 0; i < ingredients.length; i++) {
   let itm = document.createElement("li");
